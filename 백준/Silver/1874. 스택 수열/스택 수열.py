b9d6ddabb6357c35e701ac0d8ef9b54a5935e46d
hojuna <102869513+hojuna@users.noreply.github.com>
2023-04-18 11:24:19 +0900

    [Silver II] Title: 스택 수열, Time: 160 ms, Memory: 41152 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,34 +1,33 @@
-n=int(input())
+import sys
+
+def sol():
+    n, *target = map(int, sys.stdin.buffer.read().splitlines())
+    stack=[]
+    print_list=[]
 
 
-stack=[]
-target=[]
-print_list=[]
+    temp=0
+    for i in range(1,n+1):
+        stack.append(i)
+        print_list.append("+")
+        while True:
+            if stack != [ ] and stack[-1]==target[temp] and temp<n:
+                stack.pop()
+                print_list.append("-")
+                temp+=1
+            else:
+                break
 
-for i in range(n):
-    target.append(int(input()))
-
-
-temp=0
-for i in range(1,n+1):
-    stack.append(i)
-    print_list.append("+")
-    while True:
-        if stack != [ ] and stack[-1]==target[temp] and temp<n:
+    for i in range(n-temp):
+        if stack[-1]==target[temp]:
             stack.pop()
             print_list.append("-")
             temp+=1
-        else:
-            break
 
-for i in range(n-temp):
-    if stack[-1]==target[temp]:
-        stack.pop()
-        print_list.append("-")
-        temp+=1
+    if temp==n:
+        for i in print_list:
+            print(i)
+    else:
+        print('NO')
 
-if temp==n:
-    for i in print_list:
-        print(i)
-else:
-    print('NO')
+sol()
