aab5801fdfff7d75635c759f198051eed13f55a2
hojuna <102869513+hojuna@users.noreply.github.com>
2023-04-19 15:00:34 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,24 +1,11 @@
-import sys
+import sys,math
 
 def sol():
     
-    maxM=1
+    maxM=0
     minM=0
     n,m=map(int,sys.stdin.readline().split(" "))
-    maxN=max(n,m)
-
-    a=0
-    for i in range(1,maxN+1):
-        if n%i==0  and m%i==0:
-            maxM=i
-
-    k=maxN
-    while True:
-        if k%n==0  and k%m==0:
-            minM=k
-            break
-        k+=1
-    print(maxM)
-    print(minM)
+    print(math.gcd(n,m))
+    print(math.lcm(n,m))
 
 sol()
