a9fd396ba4a2aa1b3ef80d8750bffe9fbc35def2
hojuna <102869513+hojuna@users.noreply.github.com>
2023-04-21 10:44:36 +0900

    [Bronze II] Title: Hashing, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -0,0 +1,13 @@
+import sys
+
+def sol():
+    n=int(sys.stdin.readline().rstrip())
+
+    sumN=0
+    str_=sys.stdin.readline().rstrip()
+    for i in range(n):
+        k=(ord(str_[i])-96)*(31**i)
+        sumN+=k
+
+    print(sumN)
+sol()
